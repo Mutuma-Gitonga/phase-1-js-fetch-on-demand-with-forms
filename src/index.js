9212015af215edc27ed586980f25d6c695ba2e94
Mutuma Gitonga <mgitonga150@gmail.com>
2022-08-24 17:26:14 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+
+  inputForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const input = document.querySelector("input#searchByID");
+
+    fetch(`http://localhost:3002/movies/${input.value}`)
+    .then(res => res.json())
+    .then(movieData => {
+      const title = document.querySelector('section#movieDetails h4');
+      const summary = document.querySelector('section#movieDetails p');
+
+      title.textContent = movieData.title;
+      summary.textContent = movieData.summary;
+    })
+  });
+
 }
+
 
 document.addEventListener('DOMContentLoaded', init);
